1e936dddf68e3ab689ca42c95552512b0132a748
Kouhei Tsuru <kktturu1993@gmail.com>
2024-04-09 22:42:33 +0900

    :sparkles: TaskControllerに対するリソースフルルートを生成

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+
+Route::resource('tasks', TaskController::class);
 
 Route::get('/', function () {
     return view('welcome');
