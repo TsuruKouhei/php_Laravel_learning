298d212d2df9de3dee144c133e5693ea53323819
Kouhei Tsuru <kktturu1993@gmail.com>
2024-04-14 21:31:44 +0900

    :bug: viewファイルエラー修正

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -11,12 +11,12 @@
         <label for="name">タスク名:</label>
         <input type="text" name="name" id="name" required>
         <label for="deadline">期限:</label>
-        <input type="datetime-local" name="deadline" id="deadline" value="{{ old('deadline', $task->deadline ?? '') }}">
-        <label for="status">ステータス:</label>
+        <input type="datetime-local" name="deadline" id="deadline" value="{{ old('deadline', isset($task) ? $task->deadline : '') }}">
+
         <select name="status" id="status" class="form-control">
-            <option value="未着" {{ $task->status == '未着' ? 'selected' : '' }}>未着</option>
-            <option value="進行中" {{ $task->status == '進行中' ? 'selected' : '' }}>進行中</option>
-            <option value="完了" {{ $task->status == '完了' ? 'selected' : '' }}>完了</option>
+            <option value="未着" {{ (isset($task) && $task->status == '未着') ? 'selected' : '' }}>未着</option>
+            <option value="進行中" {{ (isset($task) && $task->status == '進行中') ? 'selected' : '' }}>進行中</option>
+            <option value="完了" {{ (isset($task) && $task->status == '完了') ? 'selected' : '' }}>完了</option>
         </select>
         <label for="category_id">カテゴリ:</label>
         <select name="category_id" id="category_id" class="form-control">
